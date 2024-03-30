4d9fd020014893b7759eca4f77d23398b6f68327
Wapply <88293580+Wapply@users.noreply.github.com>
2024-03-30 14:41:35 -0300

    1.0.7

---
+++ theme.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         s0urce-tor_browser-theme
+// @namespace    http://tampermonkey.net/
+// @version      1.0.7
+// @description  Modify s0urce.io to have a Tor Browser style.
+// @author       Wapply
+// @match        https://s0urce.io/*
+// @grant        GM_addStyle
+// @downloadURL https://raw.githubusercontent.com/Wapply/s0urce-tor_browser-theme/new/main/theme.js
+// @updateURL   https://raw.githubusercontent.com/Wapply/s0urce-tor_browser-theme/new/main/theme.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Add custom CSS to modify background image and element styles
+    GM_addStyle(`
+        /* Define custom CSS variables */
+        :root {
+            --color-terminal: #c567f0;
+            --color-midgreen: #9d54be;
+        }
+
+        /* Modify background image */
+        body {
+            background-image: url(https://i.imgur.com/aUfqkbN.png) !important;
+            background-size: cover !important;
+            background-repeat: no-repeat !important;
+            background-attachment: fixed !important;
+            background-position: center !important;
+        }
+
+        /* Modify styles for elements with class .green.svelte-ec9kqa */
+        .green.svelte-ec9kqa {
+            background: linear-gradient(200deg, #541a70 0%, #693780 100%) !important;
+            border: 1px solid #693680 !important;
+        }
+
+        /* Modify border color for elements with class .window-selected.svelte-1hjm43z */
+        .window-selected.svelte-1hjm43z {
+            border-color: #693780 !important;
+        }
+
+        /* Modify border style for elements with class .window.svelte-1hjm43z */
+        .window.svelte-1hjm43z {
+            border: 2px solid #693680 !important;
+        }
+
+        /* Modify height of specific div */
+        div[ondragover="return true"][draggable="true"][style="position: relative; width: 154.8px; height: 86px; font-size: 16px; float: left;"] {
+            height: 86px !important;
+        }
+    `);
+})();

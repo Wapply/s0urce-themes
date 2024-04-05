1e8fe00dbdcf10bac7cb6ec0b9e4cb39dc0787b3
Wapply <88293580+Wapply@users.noreply.github.com>
2024-04-05 19:38:40 -0300

    Create s0urce-guadleoupeans-theme.user.js

---
+++ s0urce-guadleoupeans-theme.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         s0urce-guadleoupeans-theme
+// @namespace    http://tampermonkey.net/
+// @version      1.0.9
+// @description  Official Guadloupeans Empire Theme
+// @author       Wapply & AlphaBay03
+// @match        https://s0urce.io/*
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @downloadURL https://raw.githubusercontent.com/Wapply/s0urce-themes/new/main/s0urce-guadleoupeans-theme.user.js
+// @updateURL   https://raw.githubusercontent.com/Wapply/s0urce-themes/new/main/s0urce-guadleoupeans-theme.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Add custom CSS to modify background image and element styles
+    GM_addStyle(`
+        /* Define custom CSS variables */
+        :root {
+            --color-terminal: #c567f0;
+            --color-midgreen: #9d54be;
+        }
+
+        /* Modify background image */
+        body {
+            background-image: url(https://i.imgur.com/aUfqkbN.png) !important;
+            background-size: cover !important;
+            background-repeat: no-repeat !important;
+            background-attachment: fixed !important;
+            background-position: center !important;
+        }
+
+        /* Modify styles for elements with class .green.svelte-ec9kqa */
+        .green.svelte-ec9kqa {
+            background: linear-gradient(200deg, #541a70 0%, #693780 100%) !important;
+            border: 1px solid #693680 !important;
+        }
+
+        /* Modify border color for elements with class .window-selected.svelte-1hjm43z */
+        .window-selected.svelte-1hjm43z {
+            border-color: #693780 !important;
+        }
+
+        /* Modify border style for elements with class .window.svelte-1hjm43z */
+        .window.svelte-1hjm43z {
+            border: 2px solid #693680 !important;
+        }
+
+        /* Modify height of specific div */
+        div[ondragover="return true"][draggable="true"][style="position: relative; width: 154.8px; height: 86px; font-size: 16px; float: left;"] {
+            height: 86px !important;
+        }
+    `);
+
+    // Update Spotify playlist iframe src using JavaScript
+    setInterval(() => {
+        const iframe = document.querySelector('.window-content.svelte-1hjm43z iframe');
+        if (iframe && iframe.src !== "https://open.spotify.com/embed/playlist/4nngjhtJEKEnZSdP8yW8PZ?utm_source=generator&theme=0") {
+            iframe.src = "https://open.spotify.com/embed/playlist/4nngjhtJEKEnZSdP8yW8PZ?utm_source=generator&theme=0";
+        }
+    }, 1000); // Check every 1 second
+})();
